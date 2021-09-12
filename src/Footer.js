52a960d23578243b870a8d0ecc7197c03d4ea386
chu-young <ttheko101@gmail.com>
2021-09-12 13:43:39 +0200

    changed stylesheet to modular system

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,19 @@
-import './App.css';
+import styles from './Footer.module.css';
 
 function Footer(){
 
 return(
     <footer>
-    <div class="footer-content">
+    <div className={styles.footer_content}>
         <h3>Chusyntax</h3>
         <p>Thank you for visiting my portfolio, I hope I was able to impress you. Please don't hesitate to message me on any soical platform for oppotunities or collaborations.</p>
-        <ul class="socials">
+        <ul className={styles.socials}>
             <li><a href="w"><i class="fa fa-instagram"></i></a></li>
             <li><a href="w"><i class="fa fa-linkedin-square"></i></a></li>
             <li><a href="w"><i class="fa fa-github"></i></a></li>
         </ul>
     </div>
-    <div class="footer-bottom">
+    <div className={styles.footer_bottom}>
         <p>copyright &copy;2021 Chusyntax</p>
     </div>
 </footer>
